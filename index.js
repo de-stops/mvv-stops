4bc574e36dfef04a59566e7d67770467f4ac264a
highsource <aleksei.valikov@gmail.com>
2017-02-10 00:26:49 +0100

    Filtering by RS.

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 'use strict'
 const got = require('got');
-const json2csv = require('json2csv');
-
-const endpointBase = "http://efa.mvv-muenchen.de/ng/XSLT_COORD_REQUEST?&jsonp=&boundingBox=&boundingBoxLU={minx}%3A{miny}%3AWGS84%5BDD.DDDDD%5D&boundingBoxRL={maxx}%3A{maxy}%3AWGS84%5BDD.DDDDD%5D&coordOutputFormat=WGS84%5BGGZHTXX%5D&type_1=STOP&outputFormat=json&inclFilter=1";
-const epsilon = Math.pow(2, -1);
+const csv = require('csv');
+const leftPad = require('left-pad');
+const ENDPOINT_BASE_URL_TEMPLATE = "http://efa.mvv-muenchen.de/ng/XSLT_COORD_REQUEST?&jsonp=&boundingBox=&boundingBoxLU={minx}%3A{miny}%3AWGS84%5BDD.DDDDD%5D&boundingBoxRL={maxx}%3A{maxy}%3AWGS84%5BDD.DDDDD%5D&coordOutputFormat=WGS84%5BGGZHTXX%5D&type_1=STOP&outputFormat=json&inclFilter=1";
+const EPSILON = Math.pow(2, -2);
+const MAX_STOPS = 1000;
+const RS = [
+	"09162",
+	"09173",
+	"09174",
+	"09175",
+	"09177",
+	"09178",
+	"09179",
+	"09184",
+	"09188"
+].reduce((rs, r) => Object.assign(rs, { [r]: true }), {});
 
 const queryStops = function(minx, miny, maxx, maxy, callback) {
-	const url = endpointBase
+	const url = ENDPOINT_BASE_URL_TEMPLATE
 		.replace("{minx}", minx)
 		.replace("{miny}", miny)
 		.replace("{maxx}", maxx)
 		.replace("{maxy}", maxy);
 	got(url).then(response => {
 		const result = JSON.parse(response.body);
-		if (result.pins.length > 0) {
+		// console.log("Got " + result.pins.length + " results from " + url + ".");
+		if (result.pins.length > 0 && result.pins.length <= MAX_STOPS) {
 			result.pins.forEach(callback);
 		}
-		else if (maxx - minx > epsilon)
+		else if (maxx - minx > EPSILON)
 		{
 			const midx = (minx + maxx) / 2;
 			const midy = (miny + maxy) / 2;
@@ -29,39 +42,44 @@ const queryStops = function(minx, miny, maxx, maxy, callback) {
 };
 
 const exportStops = function(stops) {
-	const result = json2csv({ data: stops, fields: ["stop_id", "stop_name", "stop_lon", "stop_lat"] });
-	process.stdout.write(result);
+	csv.stringify(stops, {header: true, quotedString: true, columns: ["stop_id", "stop_name", "stop_lon", "stop_lat", "stop_code"]}, function(err, data){
+		process.stdout.write(data);
+	});
 }
 
 const retrieveStops = function(minx, miny, maxx, maxy) {
 	const stops = [];
 	const stopsById = {};
 	const callback = s => {
-		var stopId = Number(s.stateless);
-		if (stopId < 80000000) {
-		var stop = {};
-		stop.stop_id = s.stateless;
+		if (!s.attrs) { s.attrs = []; }
+		var attributes = s.attrs.reduce((attibutes, attribute) => Object.assign(attibutes, { [attribute.name]: attribute.value }), {});
+
+		var stopId = s.stateless;
+		var stop = {};	
+		stop.stop_id = stopId;
 		stop.stop_name = s.locality + ", " + s.desc;
 		var lonLat = s.coords.split(",");
 		stop.stop_lon = Number(lonLat[0]/100000);
 		stop.stop_lat = Number(lonLat[1]/100000);
+		stop.stop_code = attributes.STOP_GLOBAL_ID || "";
 
-		const existingStop = stopsById[stopId]
-		if (existingStop &&(
-			stop.stop_id !== existingStop.stop_id ||
-			stop.stop_name !== existingStop.stop_name ||
-			stop.stop_lon !== existingStop.stop_lon ||
-			stop.stop_lat !== existingStop.stop_lat)) {
-			throw new Error("Found existing stop with id " + stopId);
-		}
-		else {
-			stopsById[stopId] = stop;
-			stops.push(stop);
-		}
+		var rs = leftPad((stop.stop_code.match(/^de:(\d+):/)||['', ''])[1], 5, '0');
+		if (RS[rs]) {
+			const existingStop = stopsById[stopId]
+			if (existingStop &&(
+				stop.stop_id !== existingStop.stop_id ||
+				stop.stop_name !== existingStop.stop_name ||
+				stop.stop_lon !== existingStop.stop_lon ||
+				stop.stop_lat !== existingStop.stop_lat)) {
+			}
+			else {
+				stopsById[stopId] = stop;
+				stops.push(stop);
+			}
 		}
 	};
 	queryStops(minx, miny, maxx, maxy, callback);
-	setTimeout(function () { exportStops(stops); }, 60000);
+	setTimeout(function () { exportStops(stops); }, 150000);
 };
 
 retrieveStops(5, 47, 15, 56);
